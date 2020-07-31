5e6d93868f8ccc274b5ef991e8bc0e3d1915e5f8
MalayaliRobz <robinphilipthomas@outlook.in>
2020-07-31 23:30:31 +0530

    image not loading fix

--- app/constants/culture-qualities.js
+++ app/constants/culture-qualities.js
@@ -3,24 +3,28 @@ export const qualities = [
     id: 'craftsmanship',
     name: 'Craftsmanship',
     letter: 'c',
-    color: '#FFA800'
+    color: '#FFA800',
+    imageLoc: '/images/craftsmanship.png'
   },
   {
     id: 'happy-work-environment',
     name: 'Happy “Work” Environment',
     letter: 'h',
-    color: '#45A4EC'
+    color: '#45A4EC',
+    imageLoc: '/images/happy-work-environment.png'
   },
   {
     id: 'agility-through-empowerment',
     name: 'Agility Through Empowerment',
     letter: 'a',
-    color: '#25C16F'
+    color: '#25C16F',
+    imageLoc: '/images/agility-through-empowerment.png'
   },
   {
     id: 'true-friend-of-the-customer',
     name: 'True Friend of the Customer',
     letter: 't',
-    color: '#9B65C3'
+    color: '#9B65C3',
+    imageLoc: '/images/true-friend-of-the-customer.png'
   }
 ]
