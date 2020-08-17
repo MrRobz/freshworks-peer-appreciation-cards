a10b8ed219210500281c40da2f7cf5abffc0c697
MalayaliRobz <robinphilipthomas@outlook.in>
2020-08-18 01:36:25 +0530

    update canvas card creation logic

--- app/controllers/card.js
+++ app/controllers/card.js
@@ -3,7 +3,6 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inlineSvgFor } from 'ember-svg-jar/utils/make-svg';
-import html2canvas from 'html2canvas';
 import { inject as service } from '@ember/service';
 import { later } from '@ember/runloop';
 import resize from "ember-animated/motions/resize";
@@ -28,6 +27,12 @@ export default class CardController extends Controller {
   cardDesc = "I am giving you this because...";
 
   @tracked
+  cardFrom = "";
+
+  @tracked
+  cardTo = "";
+
+  @tracked
   showQualityInfo = false;
 
   @tracked
@@ -48,6 +53,40 @@ export default class CardController extends Controller {
     insertedSprites.forEach(sprite => {
       fadeIn(sprite);
     });
+  }
+
+  roundRect = (ctx, x, y, width, height, radius, fill, stroke) => {
+    if (typeof stroke === 'undefined') {
+      stroke = true;
+    }
+    if (typeof radius === 'undefined') {
+      radius = 5;
+    }
+    if (typeof radius === 'number') {
+      radius = {tl: radius, tr: radius, br: radius, bl: radius};
+    } else {
+      var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+      for (var side in defaultRadius) {
+        radius[side] = radius[side] || defaultRadius[side];
+      }
+    }
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    if (fill) {
+      ctx.fill();
+    }
+    if (stroke) {
+      ctx.stroke();
+    }
   }
 
   get clipboardNotSupported() {
@@ -85,42 +124,97 @@ export default class CardController extends Controller {
     context.fillText(line, x, y);
   }
 
+  loadImage(url) {
+    return new Promise(resolve => {
+      const image = new Image();
+      image.addEventListener('load', () => {
+        resolve(image);
+      });
+      image.src = url;
+    });
+  }
+
   async generateCanvasFromDiv()  {
+    let canvas = document.createElement('canvas');
+    canvas.id = 'someId';
+    canvas.style.width = "1100px";
+    canvas.style.height = "600px";
+    document.querySelector(".dummy-wrapper").innerHTML = "";
+    document.querySelector(".dummy-wrapper").appendChild(canvas);
+
+    let ctx = canvas.getContext("2d");
+
+    let dpr = 2;
+    let rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+
+    ctx.scale(dpr, dpr);
+
+    ctx.fillStyle = "#ffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+    ctx.strokeStyle = "#E8E8E8";
+    ctx.fillStyle = "#ffff";
+    this.roundRect(ctx, 50, 50, 1000, 500, 8, true);
+
+    let base_image = await this.loadImage(this.model.lineImageLoc);
+    ctx.drawImage(base_image, 70, 155, 264, 264);
+    
+    let parser = new DOMParser();
     // eslint-disable-next-line no-undef
     let oldTopLeftIcon = inlineSvgFor('card-top-left', (assetId) => require(`ember-svg-jar/inlined/${assetId}`).default, { class: "card-icon-top-left card-icon--top-left"});
     let newTopLeftIcon = oldTopLeftIcon.replace(/fill="currentcolor"/g, `fill="${this.model.color}"`);
     newTopLeftIcon = newTopLeftIcon.replace(/stroke="currentcolor"/g, `stroke="${this.model.color}"`);
+    let svg1 = parser.parseFromString(newTopLeftIcon, "image/svg+xml");
+    let xml = new XMLSerializer().serializeToString(svg1);
+    let svg64 = btoa(xml);
+    let b64Start = 'data:image/svg+xml;base64,';
+    let image64 = b64Start + svg64;
+    
+    let iamge1 = await this.loadImage(image64);
+    ctx.drawImage(iamge1, 50, 50, 120, 120);
+
+
     // eslint-disable-next-line no-undef
     let oldTopBottomIcon = inlineSvgFor('card-bottom-left', (assetId) => require(`ember-svg-jar/inlined/${assetId}`).default, { class: "card-icon-bottom-left card-icon card-icon--bottom-left"});
     let newTopBottomIcon = oldTopBottomIcon.replace(/fill="currentcolor"/g, `fill="${this.model.color}"`);
     newTopBottomIcon = newTopBottomIcon.replace(/stroke="currentcolor"/g, `stroke="${this.model.color}"`);
+    let svg2 = parser.parseFromString(newTopBottomIcon, "image/svg+xml");
+    xml = new XMLSerializer().serializeToString(svg2);
+    svg64 = btoa(xml);
+    b64Start = 'data:image/svg+xml;base64,';
+    image64 = b64Start + svg64;
+    let image2 = await this.loadImage(image64);
+    ctx.drawImage(image2, 50, 425, 180, 125);
 
-    let node = document.querySelector('.appreciation-card');
-    let clone = node.cloneNode(true);
+    ctx.font = "700 30px Source Sans Pro";
+    ctx.fillStyle = "#474747";
+    ctx.fillText(this.model.name, 450, 100);
 
-    let appreciationTextDesc = clone.querySelector('.card-details-desc textarea').value.substring(0, CARD_DESC_CHAR_LIMIT);
-    clone.querySelector('.card-details-desc textarea').value = "";
-    clone.querySelector(".card-icon-top-left").outerHTML = newTopLeftIcon;
-    clone.querySelector(".card-icon-bottom-left").outerHTML = newTopBottomIcon;
-
-    let dummyDiv = document.querySelector('#dummy-div');
-    dummyDiv.style.display = "flex"
-    dummyDiv.appendChild(clone);
-
-    let canvas = await html2canvas(dummyDiv);
-    let ctx = canvas.getContext("2d");
-    
     ctx.font = "25px Patrick Hand";
     ctx.fillStyle = "#52575C";
-    this.wrapText(ctx, appreciationTextDesc, 400, 310, 600, 40);
+    ctx.fillText("To", 400, 150);
+    this.wrapText(ctx, this.cardDesc, 400, 210, 600, 40);
+    ctx.fillText("From", 400, 520);
 
+    ctx.font = "28px Patrick Hand";
+    ctx.fillStyle = "#000";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(this.cardTo, 440, 155);
+    ctx.fillText(this.cardFrom, 460, 530);
+    
+    ctx.fillStyle = "#DBDDE0";
+    let text = ctx.measureText(this.cardTo);
+    ctx.fillRect(433, 155, text.width + 20, 2);
+    text = ctx.measureText(this.cardFrom);
+    ctx.fillRect(453, 530, text.width + 20, 2);
+    
     ctx.font = "16px Patrick Hand";
     ctx.fillStyle = "#CACCCF";
     let host = window.location.host;
-    ctx.fillText(`Built with ${host}`, 50, 650);
-
-    document.querySelector("#dummy-div").style.display = "none"
-    document.querySelector("#dummy-div").removeChild(clone);
+    ctx.fillText(`Built with ${host}`, 52, 570);
 
     return { canvas, ctx };
   }
